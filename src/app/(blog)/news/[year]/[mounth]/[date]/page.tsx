4aaae5dfd87a9e9c6854b88101a414af920f6bbe
KIDSTKD <kidstkd.ru@gmail.com>
2024-05-21 08:27:44 +1000

    1

--- src/app/(blog)/news/[year]/[mounth]/[date]/page.tsx
+++ src/app/(blog)/news/[year]/[mounth]/[date]/page.tsx
@@ -1,10 +1,58 @@
-import Text from "./text";
+import { use } from 'react'
+import Image from 'next/image';
 
-export default function News() {
+import PocketBase from "pocketbase";
+export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
+
+interface INews {
+  id: string;
+  EventYear: number;
+  EventMounth: string;
+  EventDays: string;
+  EventDate: string;
+  EventName: string;
+  EventDescription: string;
+  EventText: string;
+  image: string;
+}
+
+export async function getNews() {
+  pbClient.autoCancellation(false)
+  const results = await pbClient.collection('03_news').getFirstListItem<INews>('EventYear="2023"', {
+  });
+
+
+  return results;
+
+};
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 1
+
+
+const NewsComponent = () => {
+
+  const res = use(getNews())
+
+
+
   return (
     <>
-      Тест
-      <Text />
+      <h1>{res.EventName}</h1>
+
+
+      <article key={res.id}>
+        <Image src={pbClient.files.getUrl(res, res.image, { 'thumb': '360x240' })} alt={res.EventName} width={360} height={240}
+          className='w-80 float-right lg:p-2 rounded aspect-[3/2] object-cover' />
+        <div dangerouslySetInnerHTML={{ __html: res.EventText }} />
+
+      </article>
     </>
   );
 }
+
+export default NewsComponent;
+
+
+
+{/* <div dangerouslySetInnerHTML={{ __html: news.EventText }} /> */ }
