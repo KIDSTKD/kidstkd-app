a18e66d01e395acaf5d5f2fe904410b3cafaa2c9
KIDSTKD <kidstkd.ru@gmail.com>
2024-05-20 13:00:29 +1000

    test2

--- src/app/(blog)/news/[year]/[mounth]/[date]/page.tsx
+++ src/app/(blog)/news/[year]/[mounth]/[date]/page.tsx
@@ -1,21 +1,10 @@
+import Text from "./text";
 
-
-export default function News({ params }: {
-  params: {
-    year: number,
-    mounth: string,
-    date: string
-  },
-}) {
-
-
-
+export default function News() {
   return (
-    <div>
-      новость {params.year}, {params.mounth}, {params.date}
-      {/* <div dangerouslySetInnerHTML={{ __html: res.EventText }} /> */}
-
-    </div>
-  )
+    <>
+      Тест
+      <Text />
+    </>
+  );
 }
-
