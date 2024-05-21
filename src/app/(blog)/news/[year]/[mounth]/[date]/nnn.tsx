9ccf262ea975670680ef379798aeb0ffbfa75d05
KIDSTKD <kidstkd.ru@gmail.com>
2024-05-21 12:58:32 +1000

    nnn

---
+++ src/app/(blog)/news/[year]/[mounth]/[date]/nnn.tsx
@@ -0,0 +1,60 @@
+import { use } from 'react'
+import Image from 'next/image';
+
+import PocketBase from "pocketbase";
+export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
+
+interface INews {
+  id: string;
+  EventYear: number;
+  EventMounth: string;
+  EventDays: string;
+  EventDate: string;
+  EventName: string;
+  EventDescription: string;
+  EventText: string;
+  image: string;
+  Date: string
+}
+
+export async function getNews() {
+  pbClient.autoCancellation(false)
+  const results = await pbClient.collection('03_news').getFirstListItem<INews>('id="0epkrrxfdcbyzux"', {
+  });
+
+
+  return results;
+
+};
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 1
+
+
+const NNN = () => {
+
+  const res = use(getNews())
+
+
+
+  return (
+    <>
+      <h1>{res.EventName}</h1>
+
+
+      <article key={res.id}>
+        <Image src={pbClient.files.getUrl(res, res.image, { 'thumb': '360x240' })} alt={res.EventName} width={360} height={240}
+          className='w-80 float-right lg:p-2 rounded aspect-[3/2] object-cover' />
+        <div dangerouslySetInnerHTML={{ __html: res.EventText }} />
+        <p>{res.Date}</p>
+
+      </article>
+    </>
+  );
+}
+
+export default NNN;
+
+
+
+{/* <div dangerouslySetInnerHTML={{ __html: news.EventText }} /> */ }
