dc1d5ef1fb3441542499224876b5b7565f8f8c6c
KIDSTKD <kidstkd.ru@gmail.com>
2024-05-23 13:46:21 +1000

    12121

--- src/app/(blog)/news/[id]/single-news.tsx
+++ src/app/(blog)/news/[id]/single-news.tsx
@@ -5,13 +5,14 @@ import PocketBase from "pocketbase";
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
 
 interface INews {
+  map(arg0: (news: INews) => import("react").JSX.Element): unknown;
   id: string;
 
   EventName: string;
   EventDescription: string;
   EventText: string;
   image: string;
-  gallary: string;
+  gallary: string[];
   Date: string
 }
 
@@ -52,6 +53,13 @@ const SingleNews = ({ newsId }: { newsId: string }) => {
             className='w-80 float-right lg:p-2 rounded aspect-[3/2] object-cover' />
           <Image src={pbClient.files.getUrl(res, res.gallary[4], { 'thumb': '360x240' })} alt={res.EventName} width={360} height={240}
             className='w-80 float-right lg:p-2 rounded aspect-[3/2] object-cover' />
+
+          {res.map((res: INews) => (
+            <>
+              {res.gallary}
+            </>
+
+          ))}
         </>
 
       </div>
