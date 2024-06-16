72f4336117864dce21a540e877870bf573f44ce2
KIDSTKD <kidstkd.ru@gmail.com>
2024-06-16 19:50:24 +1000

    12

--- src/app/(generel)/(navigation)/03-news/page.tsx
+++ src/app/(generel)/(navigation)/03-news/page.tsx
@@ -4,6 +4,16 @@ import PaginatedNews from "@/components/news-components/paginated-news";
 export const dynamic = 'force-dynamic'
 export const revalidate = 1
 
+import { Metadata } from 'next'
+const headerTitle = 'Новости'
+export const metadata: Metadata = {
+  title: headerTitle,
+  description: headerTitle + ' - KIDSTKD',
+  alternates: {
+    canonical: 'https://kidstkd.ru/03-news/',
+  },
+}
+
 export default function News({ searchParams }: any) {
 
   const page = searchParams.page
