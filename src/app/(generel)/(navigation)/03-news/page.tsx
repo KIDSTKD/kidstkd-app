7b20d76a96700222a2d812c1d14a9ff24b37580f
KIDSTKD <kidstkd.ru@gmail.com>
2024-06-16 13:24:41 +1000

    1

--- src/app/(generel)/(navigation)/03-news/page.tsx
+++ src/app/(generel)/(navigation)/03-news/page.tsx
@@ -1,21 +1,16 @@
 
 import PaginatedNews from "@/components/news-components/paginated-news";
 
-import SearchBar from "@/hooks/search-bar";
-
 export const dynamic = 'force-dynamic'
 export const revalidate = 1
 
 export default function News({ searchParams }: any) {
 
-  const aaa = searchParams.page
+  const page = searchParams.page
 
   return (
     <>
-      <PaginatedNews pagenumber={aaa} perpage={12} />
-
-      {aaa}
-
+      <PaginatedNews pagenumber={page} perpage={12} />
     </>
   );
 }
