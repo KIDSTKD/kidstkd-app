a8cbbf6a04715040b30ce50cd123c2ac9ee8e50c
KIDSTKD <kidstkd.ru@gmail.com>
2024-06-17 13:06:11 +1000

    up

--- src/app/(events)/@eventsnav/default.tsx
+++ src/app/(events)/@eventsnav/default.tsx
@@ -1,9 +1,3 @@
-import NavMenuItem from "@/layout/nav/nav-menu-item";
-import Logo from "@/components/logo";
-import CloseNav from "@/layout/nav/close-munu-button";
-
-import { PrivateMenuData } from "@/layout/nav/navigation-data";
-
 export default function EventNavigation() {
 
     return (
