0f869e0e9f9b50c79760941f601d342523a4e3a0
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-03 15:53:44 +1000

    11

--- src/app/(generel)/(blog)/kibon/[id]/page.tsx
+++ src/app/(generel)/(blog)/kibon/[id]/page.tsx
@@ -1,19 +1,19 @@
 import Kibon from "@/components/kibon-components/single-kibon";
 import KibonNavigation from "@/components/kibon-components/kibon-navigation";
 
-interface PostProps {
-   params: {
-      id: string;
-   };
-}
+// interface PostProps {
+//    params: {
+//       id: string;
+//    };
+// }
 
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export async function generateMetadata({ params }: PostProps): Promise<Metadata> {
-   return {
-      title: params.id,
-   };
-}
+// export async function generateMetadata({ params }: PostProps): Promise<Metadata> {
+//    return {
+//       title: params.id,
+//    };
+// }
 
 export default function KibonPage({ params }: { params: { id: string } }) {
    return (
