193e2fc5f05c6f8d9e7c1853561e81249cb0574b
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-03 15:30:14 +1000

    111

--- src/app/(generel)/(blog)/kibon/[id]/page.tsx
+++ src/app/(generel)/(blog)/kibon/[id]/page.tsx
@@ -1,26 +1,7 @@
 import Kibon from "@/components/kibon-components/single-kibon";
 import KibonNavigation from "@/components/kibon-components/kibon-navigation";
 
-import { getKibon } from "./get-kibon";
 
-import { Metadata } from "next";
-
-type Props = {
-   params: { id: string };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-   // read route params
-   const id = params.id;
-
-   // fetch data
-   // const product = await fetch(`https://.../${id}`).then((res) => res.json())
-   const post = getKibon(params);
-
-   return {
-      title: (await post).kibon,
-   };
-}
 
 export default function KibonPage({ params }: { params: { id: string } }) {
    return (
