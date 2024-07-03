8d0bbcdb07dcd9bf6dd8875e7b02cae61702940e
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-03 15:10:01 +1000

    test

--- src/app/(generel)/(blog)/kibon/[id]/page.tsx
+++ src/app/(generel)/(blog)/kibon/[id]/page.tsx
@@ -1,27 +1,48 @@
 import Kibon from "@/components/kibon-components/single-kibon";
 import KibonNavigation from "@/components/kibon-components/kibon-navigation";
+import { use } from 'react'
 
-import { IKibon } from "@/interfaces/kibon";
+import { Metadata } from "next"
+
+import { IKibon } from '@/interfaces/kibon';
 
 import PocketBase from "pocketbase";
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
 
-type Props = {
-   params: { id: string };
+
+export async function getKibon(params: {id: string}) {
+  pbClient.autoCancellation(false)
+  const results = await pbClient.collection('kibon').getOne<IKibon>(`${params.id}`, {
+  });
+  return results;
 };
 
-import { Metadata } from "next";
+interface PostProps {
+  params: {
+      id: any,
+  }
+}
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-   const results = await pbClient.collection("kibon").getOne<IKibon>(`${params.id}`, {});
+export async function generateMetadata({
+  params,
+}: PostProps): Promise<Metadata> {
+  const post = use(getKibon(params.id))
 
-   return {
-      title: `${results.kibon}`,
-      description: `Базовая техника - ${results.kibon_group} - ${results.kibon}`,
-      alternates: {
-         canonical: `https://kidstkd.ru/kibon/${params.id}`,
+
+
+  return {
+      title: post.kibon,
+      description: post.description,
+      openGraph: {
+          images: '/img/05/kibon/kibon.png',
+          title: post.kibon,
+          description: post.description,
+          url: 'https://kidstkd.ru/kibon' + post.id,
       },
-   };
+      alternates: {
+          canonical: 'https://kidstkd.ru/kibon' + post.id,
+      },
+  }
 }
 
 export default function KibonPage({ params }: { params: { id: string } }) {
