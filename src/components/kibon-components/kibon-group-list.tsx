097873ddcfd16dcc119bc209fbb62d46d0c2a322
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-04 21:03:50 +1000

    12

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -8,19 +8,19 @@ import PocketBase from "pocketbase";
 
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
 
-export async function getKibon(kisul_group: string, kisul: string) {
-   pbClient.autoCancellation(false);
-   const results = await pbClient.collection("kibon_schema").getFullList<IKibon>({
-      requestKey: "kibon_schema",
-      sort: "id",
-      filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
-   });
+// export async function getKibon(kisul_group: string, kisul: string) {
+//    pbClient.autoCancellation(false);
+//    const results = await pbClient.collection("kibon_schema").getFullList<IKibon>({
+//       requestKey: "kibon_schema",
+//       sort: "id",
+//       filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
+//    });
 
-   return results;
-}
+//    return results;
+// }
 
-export const dynamic = "force-dynamic";
-export const revalidate = 1;
+// export const dynamic = "force-dynamic";
+// export const revalidate = 1;
 
 // const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: string }) => {
 //    const res = use(getKibon(kisul_group, kisul));
@@ -40,22 +40,21 @@ export const revalidate = 1;
 
 // export default KibonGroupList;
 
-
 const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: string }) => {
-      const res = use(getKibon(kisul_group, kisul));
+   // const res = use(getKibon(kisul_group, kisul));
 
    return (
-         <div className="grid grid-cols-1 lg:grid-cols-2">
-            {/* {res.map((kibon: IKibon) => (
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+         {/* {res.map((kibon: IKibon) => (
                <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
                   <h5 className="cursor-pointer text-left indent-8">{kibon.id}</h5>
                   <KibonList kibon_group={kibon.id} />
                </div>
               
               
-            ))} */}11
-         </div>
-
+            ))} */}
+         11
+      </div>
    );
 };
 
