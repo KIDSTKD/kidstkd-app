b190f87c916c824bc5594a1af984b0639f6daaa7
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-04 20:15:35 +1000

    11

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -29,8 +29,8 @@ const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: st
          <div className="grid grid-cols-1 lg:grid-cols-2">
             {res.map((kibon: IKibon) => (
                <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
-                  <h5 className="cursor-pointer text-left indent-8">{kibon.kibon_group}</h5>
-                  <KibonList kibon_group={kibon.kibon_group} />
+                  <h5 className="cursor-pointer text-left indent-8">{kibon.id}</h5>
+                  <KibonList kibon_group={kibon.id} />
                </div>
             ))}
          </div>
