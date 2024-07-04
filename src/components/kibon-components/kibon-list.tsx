f613409a8b8f0d92dab257d4efa7434e11d63a57
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-04 20:28:39 +1000

    q233

--- src/components/kibon-components/kibon-list.tsx
+++ src/components/kibon-components/kibon-list.tsx
@@ -11,7 +11,7 @@ export async function getKibon(kibon_group: string) {
    pbClient.autoCancellation(false);
    const results = await pbClient.collection("kibon").getFullList<IKibon>({
       requestKey: "kibon",
-      sort: "kibon_group",
+      sort: "id",
       filter: `kibon_group = "${kibon_group}"`,
    });
 
