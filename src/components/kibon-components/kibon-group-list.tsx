0090a67f58c7830fc9845d429ceb184c8605a671
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-04 21:07:01 +1000

    121

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -1,61 +1,36 @@
 import { use } from "react";
-
 import KibonList from "./kibon-list";
-
 import { IKibon } from "@/interfaces/kibon";
-
 import PocketBase from "pocketbase";
-
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
+export async function getKibon(kisul_group: string, kisul: string) {
+   pbClient.autoCancellation(false);
+   const results = await pbClient.collection("kibon_schema").getFullList<IKibon>({
+      requestKey: "kibon_schema",
+      sort: "id",
+      filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
+   });
+   return results;
+}
+export const dynamic = "force-dynamic";
+export const revalidate = 1;
 
-// export async function getKibon(kisul_group: string, kisul: string) {
-//    pbClient.autoCancellation(false);
-//    const results = await pbClient.collection("kibon_schema").getFullList<IKibon>({
-//       requestKey: "kibon_schema",
-//       sort: "id",
-//       filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
-//    });
-
-//    return results;
-// }
-
-// export const dynamic = "force-dynamic";
-// export const revalidate = 1;
-
-// const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: string }) => {
-//    const res = use(getKibon(kisul_group, kisul));
-//    return (
-//       <>
-//          <div className="grid grid-cols-1 lg:grid-cols-2">
-//             {res.map((kibon: IKibon) => (
-//                <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
-//                   <h5 className="cursor-pointer text-left indent-8">{kibon.id}</h5>
-//                   <KibonList kibon_group={kibon.id} />
-//                </div>
-//             ))}
-//          </div>
-//       </>
-//    );
-// };
-
-// export default KibonGroupList;
 
 const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: string }) => {
-   // const res = use(getKibon(kisul_group, kisul));
+   const res = use(getKibon(kisul_group, kisul));
 
-   return (
+return (
       <div className="grid grid-cols-1 lg:grid-cols-2">
-         {/* {res.map((kibon: IKibon) => (
-               <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
-                  <h5 className="cursor-pointer text-left indent-8">{kibon.id}</h5>
-                  <KibonList kibon_group={kibon.id} />
-               </div>
-              
-              
-            ))} */}
-         11
+         {res.map((kibon: any) => (
+            // <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
+            //    <h5 className="cursor-pointer text-left indent-8">{kibon.id}</h5>
+            //    <KibonList kibon_group={kibon.id} />
+            // </div>
+            <div key={kibon.id}>
+            {kibon.id}
+            </div>
+         ))}
       </div>
-   );
+);
 };
-
 export default KibonGroupList;
