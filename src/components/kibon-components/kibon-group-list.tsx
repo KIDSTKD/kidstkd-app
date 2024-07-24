d195fc908eff58cc6cf20090ae24e2dce705c37f
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-24 17:19:50 +1000

    3e

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 import { IKibon } from "@/interfaces/kibon";
 import PocketBase from "pocketbase";
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
-export async function getKibon(kibon_group: string) {
+export async function getKibon(kisul_group: string, kisul: string) {
    pbClient.autoCancellation(false);
    const results = await pbClient.collection("kibon").getFullList<IKibon>({
       requestKey: "kibon",
       sort: "kibon_group",
-      filter: `kibon_group = "${kibon_group}"`,
+      filter: `kibon_group = "${kisul_group}"`,
    });
    return results;
 }
@@ -16,8 +16,8 @@ export async function getKibon(kibon_group: string) {
 export const dynamic = "force-dynamic";
 export const revalidate = 3600;
 
-const KibonList = ({ kibon_group }: { kibon_group: string }) => {
-   const res = use(getKibon(kibon_group));
+const KibonList = ({ kisul_group, kisul }: { kisul_group: string, kisul: string }) => {
+   const res = use(getKibon(kisul_group, kisul));
    return (
       <>
          {res.map((kibon: IKibon) => (
