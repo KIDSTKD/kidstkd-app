ec468faa7c5364c90cc48166b9e6ed7a03961cd6
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-24 17:15:14 +1000

    12

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -1,36 +1,31 @@
 import { use } from "react";
-import KibonList from "./kibon-list";
+import Link from "next/link";
 import { IKibon } from "@/interfaces/kibon";
 import PocketBase from "pocketbase";
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
-export async function getKibon(kisul_group: string, kisul: string) {
+export async function getKibon(kibon_group: string) {
    pbClient.autoCancellation(false);
    const results = await pbClient.collection("kibon").getFullList<IKibon>({
-      requestKey: "kibon_list",
-      sort: "id",
-      filter: `kisul = "${kisul}"`,
+      requestKey: "kibon",
+      sort: "kibon_group",
+      filter: `kibon_group = "${kibon_group}"`,
    });
    return results;
 }
 
-// export const dynamic = "force-dynamic";
-// export const revalidate = 3600;
+export const dynamic = "force-dynamic";
+export const revalidate = 3600;
 
-const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: string }) => {
-   const res = use(getKibon(kisul_group, kisul));
+const KibonList = ({ kibon_group }: { kibon_group: string }) => {
+   const res = use(getKibon(kibon_group));
    return (
       <>
-         <div className="grid grid-cols-1 lg:grid-cols-2">
-            {res.map((kibon: IKibon) => (
-               <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
-                  <h5 className="cursor-pointer text-left indent-8">{kibon.kibon}</h5>
-                  <KibonList kibon_group={kibon.kibon_group} />
-               </div>
-            ))}
-            
-            
-         </div>
+         {res.map((kibon: IKibon) => (
+            <Link href={`/kibon/${kibon.id}`} key={kibon.kibon_group}>
+               <p className="indent-0 pl-14 cursor-pointer hover:underline">{kibon.kibon}</p>
+            </Link>
+         ))}
       </>
    );
 };
-export default KibonGroupList;
+export default KibonList;
