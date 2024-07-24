5901abcf0026f2a5f5a7b88141259841d4fda8b2
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-24 16:09:21 +1000

    test2

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -2,30 +2,33 @@ import { use } from "react";
 import KibonList from "./kibon-list";
 import { IKibon } from "@/interfaces/kibon";
 import PocketBase from "pocketbase";
-// export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
-// export async function getKibon(kisul_group: string, kisul: string) {
-//    pbClient.autoCancellation(false);
-//    const results = await pbClient.collection("kibon_schema").getFullList<IKibon>({
-//       requestKey: "kibon_schema",
-//       sort: "id",
-//       filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
-//    });
-//    return results;
-// }
+export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
+export async function getKibon(kisul_group: string, kisul: string) {
+   pbClient.autoCancellation(false);
+   const results = await pbClient.collection("kibon_schema").getFullList<IKibon>({
+      requestKey: "kibon_schema",
+      sort: "id",
+      filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
+   });
+   return results;
+}
 
-// export const dynamic = "force-dynamic";
-// export const revalidate = 1;
+export const dynamic = "force-dynamic";
+export const revalidate = 1;
 const KibonGroupList = ({ kisul_group, kisul }: { kisul_group: string; kisul: string }) => {
-   // const res = use(getKibon(kisul_group, kisul));
+   const res = use(getKibon(kisul_group, kisul));
    return (
       <>
          <div className="grid grid-cols-1 lg:grid-cols-2">
-            {/* {res.map((kibon: IKibon) => (
+            {res.map((kibon: IKibon) => (
                <div key={kibon.id} className=" hover:bg-subblue hover:bg-opacity-25 rounded">
                   <h5 className="cursor-pointer text-left indent-8">{kibon.id}</h5>
                   <KibonList kibon_group={kibon.id} />
                </div>
-            ))} */}111 {kisul_group} - {kisul}
+            ))}
+            
+            
+            {/* 111 {kisul_group} - {kisul} */}
          </div>
       </>
    );
