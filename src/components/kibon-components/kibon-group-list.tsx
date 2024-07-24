32bba9e3cfd868bb904fb6d0cfffe810a9af376f
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-24 16:51:26 +1000

    kg

--- src/components/kibon-components/kibon-group-list.tsx
+++ src/components/kibon-components/kibon-group-list.tsx
@@ -8,7 +8,7 @@ export async function getKibon(kisul_group: string, kisul: string) {
    const results = await pbClient.collection("kibon").getFullList<IKibon>({
       requestKey: "kibon_list",
       sort: "kibon_group",
-      filter: `kisul_group = "${kisul_group}" && kisul = "${kisul}"`,
+      filter: `kisul = "${kisul}"`,
    });
    return results;
 }
