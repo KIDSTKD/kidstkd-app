ae71b0c25de06c065d14b2458ad9c5a7f8e2d3cc
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-24 23:05:42 +1000

    11

--- src/app/(generel)/(navigation)/02-docs/page.tsx
+++ src/app/(generel)/(navigation)/02-docs/page.tsx
@@ -1,6 +1,6 @@
 import Heading from "@/ui/heading";
 import DocBlock from "@/components/docs-components/docBlock";
-import Button from "@/components/ui/button";
+import Button from "@/ui/button";
 
 import { Metadata } from 'next'
 import Link from "next/link";
