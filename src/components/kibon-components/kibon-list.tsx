039c00f1965ed4cdff1b4094cbff0983286551c6
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-24 17:39:57 +1000

    2344

--- src/components/kibon-components/kibon-list.tsx
+++ src/components/kibon-components/kibon-list.tsx
@@ -4,7 +4,7 @@ import { IKibon } from "@/interfaces/kibon";
 import PocketBase from "pocketbase";
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
 export async function getKibon(kibon_group: string) {
-   pbClient.autoCancellation(false);
+   // pbClient.autoCancellation(false);
    const results = await pbClient.collection("kibon").getFullList<IKibon>({
       requestKey: "kibon",
       sort: "kibon_group",
@@ -13,8 +13,8 @@ export async function getKibon(kibon_group: string) {
    return results;
 }
 
-export const dynamic = "force-dynamic";
-export const revalidate = 3600;
+// export const dynamic = "force-dynamic";
+// export const revalidate = 3600;
 
 const KibonList = ({ kibon_group }: { kibon_group: string }) => {
    const res = use(getKibon(kibon_group));
