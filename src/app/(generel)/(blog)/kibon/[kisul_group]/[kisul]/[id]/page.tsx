a66f270404615fb51beb5c9bf0d496418030e581
KIDSTKD <kidstkd.ru@gmail.com>
2024-07-25 23:00:52 +1000

    1222

--- src/app/(generel)/(blog)/kibon/[kisul_group]/[kisul]/[id]/page.tsx
+++ src/app/(generel)/(blog)/kibon/[kisul_group]/[kisul]/[id]/page.tsx
@@ -5,36 +5,36 @@ import { IKibon } from '@/interfaces/kibon';
 import PocketBase from "pocketbase";
 export const pbClient = new PocketBase("https://kidstkd.pockethost.io");
 
-interface PostProps {
-   params: {
-      id: string;
-      kisul_group: string;
-      kisul: string;
-   };
-}
+// interface PostProps {
+//    params: {
+//       id: string;
+//       kisul_group: string;
+//       kisul: string;
+//    };
+// }
 
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export async function generateMetadata({ params }: PostProps): Promise<Metadata> {
+// export async function generateMetadata({ params }: PostProps): Promise<Metadata> {
 
-   const data = await pbClient.collection('kibon').getOne<IKibon>(`${params.id}`, {
-   });
+//    const data = await pbClient.collection('kibon').getOne<IKibon>(`${params.id}`, {
+//    });
 
-   return {
-      title: data.kibon,
-      description: "Базовая техника: " + data.kibon_group + " - " + data.kibon,
-      openGraph: {
-         images: `https://kidstkd.pockethost.io/api/files/kibon/${data?.id}/${data.img}`,
-         title: data.kibon,
-         description: "Базовая техника: " + data.kibon_group + " - " + data.kibon,
-         url: 'https://kidstkd.ru/' + params.kisul_group + "/" + params.kisul + "/" + params.id,
-     },
-     alternates: {
-      canonical: 'https://kidstkd.ru/' + params.kisul_group + "/" + params.kisul + "/" + params.id,
-  },
+//    return {
+//       title: data.kibon,
+//       description: "Базовая техника: " + data.kibon_group + " - " + data.kibon,
+//       openGraph: {
+//          images: `https://kidstkd.pockethost.io/api/files/kibon/${data?.id}/${data.img}`,
+//          title: data.kibon,
+//          description: "Базовая техника: " + data.kibon_group + " - " + data.kibon,
+//          url: 'https://kidstkd.ru/' + params.kisul_group + "/" + params.kisul + "/" + params.id,
+//      },
+//      alternates: {
+//       canonical: 'https://kidstkd.ru/' + params.kisul_group + "/" + params.kisul + "/" + params.id,
+//   },
 
-   };
-}
+//    };
+// }
 
 export default function KibonPage({ params }: { params: { id: string } }) {
 
