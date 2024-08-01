8217083ed2c396156ad0a0e13231ad868fa87a5e
KIDSTKD <kidstkd.ru@gmail.com>
2024-08-01 14:32:10 +1000

    434

---
+++ src/app/(generel)/(blog)/competition/og/2024/page.tsx
@@ -0,0 +1,26 @@
+
+import { Metadata } from "next";
+const headerTitle = "Тхэквондо - ОИ2024";
+export const metadata: Metadata = {
+   title: headerTitle,
+   description: "Тхэквондо на летних Олимпийских играх 2024",
+   openGraph: {
+      // images: '',
+      title: headerTitle,
+      description: "Тхэквондо на летних Олимпийских играх 2024",
+      url: "https://kidstkd.ru/competition/og/2024",
+   },
+   alternates: {
+      canonical: "https://kidstkd.ru/competition/og/2024",
+   },
+};
+
+
+
+export default function OG2024() {
+   return (
+      <>
+        
+      </>
+   );
+}
