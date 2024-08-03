fbeeca960da2441ee950e45b27dd4f26e8df5bd3
KIDSTKD <kidstkd.ru@gmail.com>
2024-08-03 13:15:50 +1000

    rrr

--- src/components/news-components/single-news.tsx
+++ src/components/news-components/single-news.tsx
@@ -24,29 +24,28 @@ const SingleNews = ({ newsId }: { newsId: string }) => {
   const res = use(getNews(newsId))
 
   return (
-    <>
+    <div className='flex flex-col'>
       <h1>{res.EventName}</h1>
 
 
-      <article key={res.id}>
+      <article key={res.id} className=''>
         <Image src={pbClient.files.getUrl(res, res.gallary[0], { 'thumb': '360x240' })} alt={res.EventName} width={360} height={240}
-          className='w-80 float-right lg:p-2 rounded aspect-[3/2] object-cover' />
+          className='w-80 float-right lg:p-2 rounded object-cover' />
         <div dangerouslySetInnerHTML={{ __html: res.EventText }} />
       </article>
-
-      <>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
         {res.gallary.map((gallary: INews) => (
           <Link key={gallary.id} href={`https://kidstkd.pockethost.io/api/files/5ivpjpyfv12nfus/${res.id}/${gallary}`}
             className='float-left'>
             <Image src={`https://kidstkd.pockethost.io/api/files/5ivpjpyfv12nfus/${res.id}/${gallary}`}
               alt={res.EventName} width={320} height={200}
-              className='w-full lg:w-80 aspect-video object-cover object-center hover:scale-105 transition-all' />
+              className='w-full aspect-video object-cover object-center hover:scale-105 transition-all' />
           </Link>
         ))}
 
-      </>
+      </div>
 
-    </>
+    </div>
   );
 }
 
