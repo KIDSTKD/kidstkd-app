e06fbe8688b272e2573f9971d306418643cf0e26
KIDSTKD <kidstkd.ru@gmail.com>
2024-08-07 18:21:01 +1000

    1

--- src/app/(generel)/(blog)/competition/og/2024/live/day1/page.tsx
+++ src/app/(generel)/(blog)/competition/og/2024/live/day1/page.tsx
@@ -22,7 +22,6 @@ export default function OG2024() {
          <Heading text={headerTitle} />
 
          <h4>Предварительные поединки</h4>
-         <iframe src="https://vk.com/video_ext.php?oid=-211669815&id=456245015&hd=2&autoplay=1" width="853" height="480" allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"></iframe>
 <iframe className="w-full aspect-video" src="//www.cbc.ca/i/phoenix/player/syndicate/?mediaId=9.6452535" width="640" height="320"></iframe>
 <h4>Полуфиналы</h4>
 <iframe className="w-full aspect-video" src="//www.cbc.ca/i/phoenix/player/syndicate/?mediaId=9.6452590" width="640" height="320"></iframe>
