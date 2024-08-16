026cd7f1930ae95a5fb7565b9db988b7d9e49b8a
KIDSTKD <kidstkd.ru@gmail.com>
2024-08-16 18:34:46 +1000

    ert

--- src/app/(generel)/(navigation)/page.tsx
+++ src/app/(generel)/(navigation)/page.tsx
@@ -13,15 +13,12 @@ import PaginatedPosts from "@/components/posts-components/paginated-posts";
 import { Metadata } from "next";
 const headerTitle = "Тхэквондо в Уссурийске";
 export const metadata: Metadata = {
-   title: headerTitle + " для детей от 4 лет",
-   description: "Спортивные секции для детей в Уссурийске. Олимпийское тхэквондо для мальчиков и девочек от 4 лет. Бесплатное пробное занятие",
+
    alternates: {
       canonical: "https://kidstkd.ru",
    },
 };
 
-export const dynamic = "force-dynamic";
-export const revalidate = 1;
 
 export default function Home() {
    return (
