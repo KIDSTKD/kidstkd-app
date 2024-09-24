c5858d609f84abb53ba304d1a2727d23f79c1924
KIDSTKD <kidstkd.ru@gmail.com>
2024-09-24 14:05:07 +1000

    55

--- src/app/(events)/events/page.tsx
+++ src/app/(events)/events/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+
+import CompLogo from '../../../../public/img/events/ussur.png'
 
 import { Metadata } from 'next'
 const headerTitle = 'Турниры'
@@ -12,8 +15,15 @@ export const metadata: Metadata = {
 export default function EventsPage() {
 
   return (
-    <>
-      events test
-    </>
+    <div className='grid grid-cols-3 gap-3 p-1'>
+    <div className='bg-light/25 hover:bg-light/5 transition-colors cursor-pointer rounded-sm'>
+<Image src={CompLogo} alt='Открытое Первенство Уссурийского ГО' className='p-5'/>
+<div className='truncate
+                      text-gray font-bebasRegular text-2xl text-center p-2 pb-0
+                      select-none cursor-pointer 
+      '>Открытое Первенство Уссурийского ГО</div>  
+      <div className='font-exo2 pt-0 pr-4 text-right'>6/10/2024 - г. Уссурйиск</div>  
+      </div>
+    </div>
   )
 }
