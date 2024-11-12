a8045e9911c291880ec274ca275668c9f324ff08
KIDSTKD <kidstkd.ru@gmail.com>
2024-11-12 15:15:45 +1000

    545

--- src/app/(events)/events/page.tsx
+++ src/app/(events)/events/page.tsx
@@ -18,6 +18,15 @@ export default function EventsPage() {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 gap-3 p-1'>
 
+<div className='bg-light/25 hover:bg-light/5 transition-colors cursor-pointer rounded-sm'>
+<Image src={FestLogo} alt='Фестиваль с.Покровка' className='p-5'/>
+<div className='truncate
+                      text-gray font-bebasRegular text-2xl text-center p-2 pb-0
+                      select-none cursor-pointer 
+      '>Фестиваль с.Покровка</div>  
+      <div className='font-exo2 pt-0 pr-4 text-right'>16/11/2024 - с. Покровка</div>  
+      </div>
+
     <div className='bg-light/25 hover:bg-light/5 transition-colors cursor-pointer rounded-sm'>
 <Image src={CompLogo} alt='Открытое Первенство Уссурийского ГО' className='p-5'/>
 <div className='truncate
@@ -27,14 +36,7 @@ export default function EventsPage() {
       <div className='font-exo2 pt-0 pr-4 text-right'>6/10/2024 - г. Уссурйиск</div>  
       </div>
 
-      <div className='bg-light/25 hover:bg-light/5 transition-colors cursor-pointer rounded-sm'>
-<Image src={FestLogo} alt='Фестиваль с.Покровка' className='p-5'/>
-<div className='truncate
-                      text-gray font-bebasRegular text-2xl text-center p-2 pb-0
-                      select-none cursor-pointer 
-      '>Фестиваль с.Покровка</div>  
-      <div className='font-exo2 pt-0 pr-4 text-right'>16/11/2024 - с. Покровка</div>  
-      </div>
+
 
     </div>
   )
